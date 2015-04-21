a40dac479cfe509e148eab07962fe54227453af5
Santiago Frias Moreno <sfrias1@gmail.com>
2015-04-21 22:23:57 +0200

    Extended Class for reals

---
+++ ClassOnline/src/org/visualia/prg/edxjav00/conplexe/ComplExtd.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.visualia.prg.edxjav00.conplexe;
+/**
+ * @author sfrias
+ */
+public class ComplExtd extends Complex {
+	private boolean esReal;
+	/**
+	 * @param par1
+	 * @param par2
+	 * @param concert
+	 */
+	public ComplExtd(double par1, double par2, boolean concert) {
+		super(par1, par2, concert);
+		esReal = par1 ==0;
+	}
+	public ComplExtd(double par1){
+		super(par1, 0, true);
+		esReal = true;
+	}
+	@Override
+	public void cxPlus(Complex csum) {
+		super.cxPlus(csum);
+		esReal = getImg() == - csum.getImg();
+	}
+	@Override
+	public String strBinomic (){
+	  if(esReal) {
+	    return getReal() + " ¡real!";
+	  } else{
+	    return super.strBinomic();
+	  }
+	}
+	public String strCartesian (){
+		  if(esReal) {
+		    return getReal() + " ¡real!";
+		  } else{
+		    return super.strCartesian();
+		  }
+		}
+	public boolean esReal(){
+		return esReal;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ClassOnline/src/org/visualia/prg/edxjav00/conplexe/Complex.java
+++ ClassOnline/src/org/visualia/prg/edxjav00/conplexe/Complex.java
@@ -4,9 +4,9 @@ import java.lang.Math;
 
 /**
  * Representacion de numeros complejos
- * Created by Frias Moren on 01/04/2015.
+ * Created by Frias Moren on 21/04/2015.
  */
-public class complex {
+public class Complex {
     // Atributos-Propiedades-Campos (items)
     private double real, img, mod, arg;
     /**
@@ -16,7 +16,7 @@ public class complex {
      * @param concert indicacion de la forma especificada
      */
     
-    public complex (double par1, double par2, boolean concert) {
+    public Complex (double par1, double par2, boolean concert) {
     	if (concert) {
     	// Parametros en forma binomica
         this.real = par1;
@@ -31,7 +31,8 @@ public class complex {
             this.arg = par2 ;
 		}
     }
-    // métodos
+    
+	// metodos
     /**@return  string en forma binómica */
     public String strBinomic (){
         return real+"+"+img+"i";
@@ -46,17 +47,42 @@ public class complex {
     }
     /**
     /**@param  csum complejo que le sumamos */
-    public void cxPlus(complex csum){
+    public void cxPlus(Complex csum){
         this.real = real+csum.real;
         this.img = img+csum.img;	
         this.mod = Math.hypot(real, img);
         this.arg = Math.atan2(img, real);
-
     }
+        
+public double getReal() {
+		return real;
+	}
+	public void setReal(double real) {
+		this.real = real;
+	}
+	public double getImg() {
+		return img;
+	}
+	public void setImg(double img) {
+		this.img = img;
+	}
+	public double getMod() {
+		return mod;
+	}
+	public void setMod(double mod) {
+		this.mod = mod;
+	}
+	public double getArg() {
+		return arg;
+	}
+	public void setArg(double arg) {
+		this.arg = arg;
+	}
+	// Metodo principal para pruebas.
     public static void main(String[] main){
-        complex z,w;
-        z = new complex (-1.5, 3.0,true);
-        w = new complex (-1.2 ,2.4,true);
+        Complex z,w;
+        z = new Complex (-1.5, 3.0,true);
+        w = new Complex (-1.2 ,2.4,true);
         z.cxPlus (w);
         System.out.println("Complejo: "+ z.strBinomic());
         System.out.println("Complejo: "+ z.strPolar());
